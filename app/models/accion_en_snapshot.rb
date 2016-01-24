760e781b934b37a54f44e63976b4e700889e7c93
Fernando Calatayud <fernandorankia@gmail.com>
2016-01-24 20:31:04 +0100

    Improve rubocop

--- app/models/accion_en_snapshot.rb
+++ app/models/accion_en_snapshot.rb
@@ -1,9 +1,9 @@
 class AccionEnSnapshot < ActiveRecord::Base
   belongs_to :accion
   belongs_to :snapshot
-  
+
   delegate :nombre, to: :accion
-  
+
   def self.crear_desde_dato(dato, i)
     create(accion: Accion.buscar_o_crear_desde_dato(dato),
            posicion: i + 1,
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :snapshots
   get 'importar_snapshot/establecer_origen' => 'snapshot_importer#establecer_origen', as: 'establecer_origen'
   post 'importar_snapshot/pide_lista_snapshots' => 'snapshot_importer#pide_lista_snapshots', as: 'pide_lista_snapshots'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
