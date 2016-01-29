2dc1056788ffd2ea4323cc7aecf7604f26294021
Fernando Calatayud <fernandorankia@gmail.com>
2016-01-29 23:53:17 +0100

    fix copyempastre on crear AccionEnSnapshot

--- app/models/accion_en_snapshot.rb
+++ app/models/accion_en_snapshot.rb
@@ -10,7 +10,7 @@ class AccionEnSnapshot < ActiveRecord::Base
 
   def self.crear_desde_dato(dato, i)
     create(accion: Accion.buscar_o_crear_desde_dato(dato),
-           posicion: dato['visitas'] || i + 1,
+           posicion: dato['posicion'] || i + 1,
            visitas_recientes: dato['visitas_recientes'],
            visitas_totales: dato['visitas'] || dato['visitas_totales'])
   end
