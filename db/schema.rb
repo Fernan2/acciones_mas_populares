af3f22c8f9a219f3f19fabefc01a43e3cacf1c46
Fernando Calatayud <fernandorankia@gmail.com>
2016-01-29 23:54:20 +0100

    accion_en_snapshots, on_update: :cascade

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123210221) do
+ActiveRecord::Schema.define(version: 20160129205455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,5 @@ ActiveRecord::Schema.define(version: 20160123210221) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accion_en_snapshots", "snapshots", on_update: :cascade
 end
